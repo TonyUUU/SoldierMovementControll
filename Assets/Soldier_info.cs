d1a02fddb29f4a4d7bc43b1bcb3e20d0c3d64c73
Chengli Wu <tonycl.wu@mail.utoronto.ca>
2018-09-23 11:03:52 -0400

    add shooting function

---
+++ Assets/Soldier_info.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldier_info : MonoBehaviour {
+    public Object_status status;
+	// Use this for initialization
+	void Start () {
+        status = new Object_status(this.gameObject, 100);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
