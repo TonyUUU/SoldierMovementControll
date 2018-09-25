ff13fe73b4df2bdf34247b127cb30e84d31224b3
Yuhao Kan <kyh649782182@gmail.com>
2018-09-24 20:08:04 -0400

    Revert "merge tony"

---
+++ Assets/Soldier_controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Soldier_controller : MonoBehaviour {
+
+    public float speed = 10.0F;
+
+    // Use this for initialization
+    void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float z_change = Input.GetAxis("Vertical") * speed;
+        float x_change = Input.GetAxis("Horizontal") * speed;
+        transform.Translate(x_change * Time.deltaTime, 0, z_change * Time.deltaTime);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+	}
+}
